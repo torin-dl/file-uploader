c61f555a61ad06c25f67d6aa7814cb3c85481558
shibe7202 <46942349+shibe7202@users.noreply.github.com>
2026-06-04 16:06:12 -0700

    Add file specific validation

--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,22 @@ import multer from "multer";
 
 const router = Router();
 
-const upload = multer({ dest: "./public/uploads/" });
+const upload = multer({
+    dest: "../public/uploads/",
+    fileFilter: (req, file, cb) => {
+        const isAllowed =
+            file.mimetype.startsWith("text/") ||
+            file.mimetype.startsWith("image/") ||
+            file.mimetype.startsWith("video/") ||
+            file.mimetype.startsWith("application/");
+        if (isAllowed) {
+            cb(null, true);
+        } else {
+            cb(new Error("File type not allowed"));
+        }
+    },
+    limits: { fileSize: 1024 * 1024 * 10 },
+});
 
 router.get("/upload", fileController.uploadGet);
 router.post("/upload", upload.single("file"), fileController.uploadPost);
